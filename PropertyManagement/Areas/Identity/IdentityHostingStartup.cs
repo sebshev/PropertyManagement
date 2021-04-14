bd23127f0093ba96c2adb0ee878369dea86421d5
Sebastian Shevlin <seb112697@gmail.com>
2021-04-13 19:09:15 -0700

    Scaffolded Login and Register pages

---
+++ PropertyManagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PropertyManagement.Data;
+
+[assembly: HostingStartup(typeof(PropertyManagement.Areas.Identity.IdentityHostingStartup))]
+namespace PropertyManagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
