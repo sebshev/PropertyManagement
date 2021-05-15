fc285388289159f8041c45fb79088a7a3e814e8c
Sebastian <66985210+sebshev@users.noreply.github.com>
2021-05-15 13:58:48 -0700

    Created property class and added index using Razor (#17)

---
+++ PropertyManagement/Controllers/PropertiesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PropertyManagement.Data;
+using PropertyManagement.Models;
+
+namespace PropertyManagement.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PropertiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Properties
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Property.ToListAsync());
+        }
+
+        // GET: Properties/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @property = await _context.Property
+                .FirstOrDefaultAsync(m => m.PropertyId == id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            return View(@property);
+        }
+
+        // GET: Properties/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Properties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PropertyId,Address,Rooms,Baths")] Property @property)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@property);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@property);
+        }
+
+        // GET: Properties/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @property = await _context.Property.FindAsync(id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+            return View(@property);
+        }
+
+        // POST: Properties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("PropertyId,Address,Rooms,Baths")] Property @property)
+        {
+            if (id != @property.PropertyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@property);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PropertyExists(@property.PropertyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@property);
+        }
+
+        // GET: Properties/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @property = await _context.Property
+                .FirstOrDefaultAsync(m => m.PropertyId == id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            return View(@property);
+        }
+
+        // POST: Properties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var @property = await _context.Property.FindAsync(id);
+            _context.Property.Remove(@property);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PropertyExists(string id)
+        {
+            return _context.Property.Any(e => e.PropertyId == id);
+        }
+    }
+}
---
+++ PropertyManagement/Models/Property.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyManagement.Models
+{
+    public class Property
+    {
+        [Key]
+        public string PropertyId { get; set; }
+        public string Address { get; set; }
+
+        public double Rooms { get; set; }
+
+        public double Baths { get; set; }
+    }
+}
