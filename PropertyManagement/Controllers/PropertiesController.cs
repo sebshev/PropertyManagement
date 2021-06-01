fb371915c5b2a03b6460c326781c71d4a4d1c310
Sebastian Shevlin <seb112697@gmail.com>
2021-06-01 10:02:58 -0700

    only realtor can create and view properties

--- PropertyManagement/Controllers/PropertiesController.cs
+++ PropertyManagement/Controllers/PropertiesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using PropertyManagement.Models;
 
 namespace PropertyManagement.Controllers
 {
+    [Authorize(Roles = IdentityHelper.Realtor)]
     public class PropertiesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -19,7 +21,6 @@ namespace PropertyManagement.Controllers
             _context = context;
         }
 
-        // GET: Properties
         public async Task<IActionResult> Index()
         {
             return View(await _context.Property.ToListAsync());
